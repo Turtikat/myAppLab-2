7bcadbc9d55e1329a523dde8f537bdf946aa61e9
Turtikat <katja.turtiainen@student.laurea.fi>
2025-03-03 15:18:33 +0200

    corr

--- src/pages/Service.tsx
+++ src/pages/Service.tsx
@@ -1,25 +1,18 @@
-// pages/Service.tsx
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 
 const Service: React.FC = () => {
- return (
-  <IonPage>
-    <IonHeader>
-    
-    <IonToolbar>
-        <IonTitle>Service</IonTitle>
-    </IonToolbar>
-</IonHeader>
-<IonContent fullscreen>
-    <IonHeader collapse="condense">
-<IonToolbar>
-    <IonTitle size="large">TabService</IonTitle>
- </IonToolbar>
-</IonHeader>
-
-   </IonContent>
- </IonPage>
-);
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Service Page</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <p>Welcome to the Service Page!</p>
+      </IonContent>
+    </IonPage>
+  );
 };
-export default Service;
 
+export default Service;
