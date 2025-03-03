5c6a48a5aecf090218759d305b3b3428890c8f8b
Turtikat <katja.turtiainen@student.laurea.fi>
2025-03-03 15:13:37 +0200

    x

--- src/pages/Tab1.tsx
+++ src/pages/Tab1.tsx
@@ -29,21 +29,16 @@ import {
   IonApp,
   IonButton,
   IonButtons,
-  IonContent,
-  IonHeader,
   IonIcon,
   IonItem,
   IonLabel,
   IonList,
   IonListHeader,
   IonMenu,
-  IonMenuToggle,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-  } from "@ionic/react";
-  import "./Tab1.css";
-  import { homeOutline, reorderThreeOutline, apertureOutline } from "ionicons/icons";
+  IonMenuToggle
+} from "@ionic/react";
+import { apertureOutline, homeOutline, reorderThreeOutline } from "ionicons/icons";
+import "./Tab1.css";
   const Tab1: React.FC = () => {
   return (
   <IonApp>
@@ -58,4 +53,38 @@ import {
   <IonListHeader>
   <IonLabel>Navigate from here..</IonLabel>
   </IonListHeader>
-  
+  <IonMenuToggle autoHide>
+<IonItem button routerLink="/tab1">
+<IonIcon slot="start" icon={homeOutline}></IonIcon>
+<IonLabel>Home</IonLabel>
+</IonItem>
+<IonItem routerLink="/service">
+<IonIcon slot="start" icon={apertureOutline}></IonIcon>
+<IonLabel>Service</IonLabel>
+</IonItem>
+</IonMenuToggle>
+</IonList>
+</IonContent>
+</IonMenu>
+<IonPage id="main-content">
+<IonHeader>
+<IonToolbar>
+<IonButtons slot="start">
+<IonMenuToggle>
+<IonButton>
+<IonIcon slot="start" icon={reorderThreeOutline}></IonIcon>
+</IonButton>
+</IonMenuToggle>
+</IonButtons>
+<IonTitle>Header</IonTitle>
+</IonToolbar>
+</IonHeader>
+<IonContent className="ion-padding">
+<h1>Main Content</h1>
+<p>Click the icon in the top left to toggle the Menu.</p>
+</IonContent>
+</IonPage>
+</IonApp>
+);
+};
+export default </Service>;
