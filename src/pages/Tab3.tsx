fd7aa654332d607dcde1008caba10e0851965c2e
Turtikat <katja.turtiainen@student.laurea.fi>
2025-03-14 18:21:58 +0200

    tab3 changes

--- src/pages/Tab3.tsx
+++ src/pages/Tab3.tsx
@@ -1,25 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Tab3.css';
+import { IonButton, IonContent, IonHeader, IonInput, IonItem, IonLabel, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
+import { useState } from 'react';
 
 const Tab3: React.FC = () => {
+  const [subject, setSubject] = useState('');
+  const [message, setMessage] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    alert(`Aihe: ${subject}\nViesti: ${message}\nSähköposti: ${email}`);
+  };
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Contact</IonTitle>
+          <IonTitle>Yhteydenotto</IonTitle>
         </IonToolbar>
       </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Tab 3</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer name="Tab 3 page" />
+      <IonContent className="ion-padding">
+        <IonItem>
+          <IonLabel position="floating">Aihe</IonLabel>
+          <IonInput value={subject} onIonChange={(e) => setSubject(e.detail.value!)} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Viesti</IonLabel>
+          <IonTextarea value={message} onIonChange={(e) => setMessage(e.detail.value!)} />
+        </IonItem>
+        <IonItem>
+          <IonLabel position="floating">Sähköpostiosoite</IonLabel>
+          <IonInput type="email" value={email} onIonChange={(e) => setEmail(e.detail.value!)} />
+        </IonItem>
+        <IonButton expand="block" onClick={handleSubmit} color="primary">
+          Lähetä viesti
+        </IonButton>
       </IonContent>
     </IonPage>
   );
 };
 
 export default Tab3;
+
